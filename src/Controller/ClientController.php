4775d2b4e717b74653cbcc376fd2ef98313c0e70
Bechir <ba.bechir@gmail.com>
2020-06-05 05:46:35 +0200

    Product detail page template static

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -10,15 +10,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ClientController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/products", name="products")
      */
-    public function showAllArticles(ArticleRepository $repo)
+    public function showAllProducts(ArticleRepository $repo)
     {
     
         $articles = $repo->findAll();
 
-        return $this->render('client/index.html.twig', [
+        return $this->render('products/products.html.twig', [
             'articles' => $articles,
+        ]);
+    }
+
+     /**
+     * @Route("/products_details", name="products_details")
+     */
+    public function showProductDetail(ArticleRepository $repo)
+    {
+
+        return $this->render('products/product_detail.html.twig', [
+           // 'articles' => $articles,
         ]);
     }
 
